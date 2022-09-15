caa70182c309de04b546e6284387c8404950b6fb
ghaoii <641517196@qq.com>
2022-09-15 22:00:12 +0800

    commit_9_15 复习

--- src/test/Test.java
+++ src/test/Test.java
@@ -7,7 +7,50 @@ import java.util.concurrent.ConcurrentHashMap;
 
 public class Test {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while(scanner.hasNextInt()) {
+            int m = scanner.nextInt();
+            int n = scanner.nextInt();
+            if(m > n) {
+                int temp = m;
+                m = n;
+                n = temp;
+            }
 
+            int sum = 0;
+            int count = 0;
+            for(int i = m; i <= n; i++) {
+                if((i / 100) % 2 == 0) {
+                    i += 99;
+                    continue;
+                }
+                if((i / 10) % 2 == 0) {
+                    i += 9;
+                    continue;
+                }
+                if((i & 1) == 1) {
+                    sum += i;
+                    count++;
+                }
+//                if(isAllOdd(i)) {
+//                    sum += i;
+//                    count++;
+//                }
+            }
+            int ret = sum == 0 ? 0 : sum / count;
+            System.out.println(ret);
+        }
+    }
+
+    private static boolean isAllOdd(int num) {
+        while(num > 0) {
+            int n = num % 10;
+            if((n & 1) == 0) {
+                return false;
+            }
+            num /= 10;
+        }
+        return true;
     }
 }
 
