5b602205dda7273fae956cf833587823a953db9c
ghaoii <641517196@qq.com>
2022-10-23 16:33:20 +0800

    commit_10_23 复习

--- src/exam/Main.java
+++ src/exam/Main.java
@@ -1,64 +1,28 @@
 package exam;
 
 import java.util.*;
-class Pair {
-    int x;
-    int y;
-
-    public Pair(int x, int y) {
-        this.x = x;
-        this.y = y;
-    }
-}
 public class Main {
-    private static int[][] next = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int row = scanner.nextInt();
-        int col = scanner.nextInt();
-        int[][] arr = new int[row][col];
-        for (int i = 0; i < row; i++) {
-            for (int j = 0; j < col; j++) {
-                arr[i][j] = scanner.nextInt();
+        while(scanner.hasNextInt()) {
+            int n = scanner.nextInt();
+            if(n == 0) {
+                return;
             }
-        }
-        Pair start = new Pair(0, 0);
-        Deque<List<Pair>> queue = new LinkedList<>();
-        List<Pair> list = new ArrayList<>();
-        list.add(start);
-        queue.offer(list);
-        int[][] book = new int[row][col];
-        book[0][0] = 1;
-        List<Pair> ret = null;
-        while(!queue.isEmpty()) {
-            int size = queue.size();
-            for (int i = 0; i < size; i++) {
-                list = queue.poll();
-                Pair cur = list.get(list.size() - 1);
-                if(cur.x == row - 1 && cur.y == col - 1) {
-                    ret = list;
-                    break;
+            if(n == 1) {
+                System.out.println(0);
+                continue;
+            }
+            int count = 0;
+            while(n > 3) {
+                if(n % 3 == 0) {
+                    n /= 3;
+                }else {
+                    n = n / 3 + 1;
                 }
-                for (int j = 0; j < 4; j++) {
-                    int newX = cur.x + next[j][0];
-                    int newY = cur.y + next[j][1];
-                    if(newX < 0 || newX >= row || newY < 0 || newY >= col ||
-                            book[newX][newY] == 1 || arr[newX][newY] == 1) {
-                        continue;
-                    }
-                    Pair pair = new Pair(newX, newY);
-                    List<Pair> temp = new ArrayList<>(list);
-                    temp.add(pair);
-                    queue.offer(temp);
-                    book[newX][newY] = 1;
-                }
+                count++;
             }
-            if(ret != null) {
-                break;
-            }
-        }
-        for(Pair pair : ret) {
-            System.out.println("(" + pair.x + "," + pair.y + ")");
+            System.out.println(++count);
         }
     }
 }
--- src/test/Test.java
+++ src/test/Test.java
@@ -7,50 +7,19 @@ import java.util.concurrent.ConcurrentHashMap;
 
 public class Test {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        while(scanner.hasNextInt()) {
-            int m = scanner.nextInt();
-            int n = scanner.nextInt();
-            if(m > n) {
-                int temp = m;
-                m = n;
-                n = temp;
-            }
-
-            int sum = 0;
-            int count = 0;
-            for(int i = m; i <= n; i++) {
-                if((i / 100) % 2 == 0) {
-                    i += 99;
-                    continue;
-                }
-                if((i / 10) % 2 == 0) {
-                    i += 9;
-                    continue;
-                }
-                if((i & 1) == 1) {
-                    sum += i;
-                    count++;
-                }
-//                if(isAllOdd(i)) {
-//                    sum += i;
-//                    count++;
-//                }
-            }
-            int ret = sum == 0 ? 0 : sum / count;
-            System.out.println(ret);
-        }
+        Test test = new Test();
+        test.show();
     }
 
-    private static boolean isAllOdd(int num) {
-        while(num > 0) {
-            int n = num % 10;
-            if((n & 1) == 0) {
-                return false;
-            }
-            num /= 10;
-        }
-        return true;
+    public static void test() {
+        System.out.println("静态方法");
     }
+
+    public void show() {
+        System.out.println("准备调用静态方法……");
+        Test test = new Test();
+        test.test();
+    }
+
 }
 
