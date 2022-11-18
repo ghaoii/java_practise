93fec71923f1ed617e0a0dfbdae018024885c928
ghaoii <641517196@qq.com>
2022-11-18 22:30:44 +0800

    commit_11_18 复习、刷题

---
+++ src/offer/Off53_Search.java
@@ -0,0 +1,34 @@
+package offer;
+
+public class Off53_Search {
+    public int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        // 找左边界
+        while(left <= right) {
+            int mid = left + (right - left) / 2;
+            if(nums[mid] >= target) {
+                right = mid - 1;
+            }else {
+                left = mid + 1;
+            }
+        }
+        if(left == nums.length || nums[left] != target) {
+            return 0;
+        }
+        int start = left;
+
+        // 找右边界
+        left = 0;
+        right = nums.length - 1;
+        while(left <= right) {
+            int mid = left + (right - left) / 2;
+            if(nums[mid] <= target) {
+                left = mid + 1;
+            }else {
+                right = mid - 1;
+            }
+        }
+        return right - start + 1;
+    }
+}
