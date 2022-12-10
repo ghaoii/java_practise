df26787a9277c3407f65ab1c435e7783ba44e48a
ghaoii <641517196@qq.com>
2022-12-10 19:12:20 +0800

    commit_12_10 刷题

--- src/interview/Num3_1_TripleInOne.java
+++ src/interview/Num3_1_TripleInOne.java
@@ -5,37 +5,45 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class Num3_1_TripleInOne {
-    List<Integer> arr;
+    int[][] arr;
+
+    int[] pos = new int[3];
 
     int maxSize;
 
     public Num3_1_TripleInOne(int stackSize) {
-        arr = new ArrayList<>(stackSize);
+        arr = new int[3][stackSize];
         maxSize = stackSize;
     }
 
     public void push(int stackNum, int value) {
-        if(stackNum < 0 || stackNum >= maxSize || arr.size() >= maxSize) {
-            return;
+        int index = pos[stackNum];
+        if(index < maxSize) {
+            arr[stackNum][index] = value;
+            pos[stackNum]++;
         }
-        arr.add(stackNum, value);
     }
 
     public int pop(int stackNum) {
-        if(stackNum < 0 || stackNum >= maxSize || arr.size() == 0) {
-            return -1;
+        int index = pos[stackNum];
+        int val = -1;
+        if(index > 0) {
+            val = arr[stackNum][index - 1];
+            pos[stackNum]--;
         }
-        return arr.remove(stackNum);
+        return val;
     }
 
     public int peek(int stackNum) {
-        if(stackNum < 0 || stackNum >= maxSize || arr.size() == 0) {
+        int index = pos[stackNum];
+        if(index > 0) {
+            return arr[stackNum][index - 1];
+        }else {
             return -1;
         }
-        return arr.get(stackNum);
     }
 
     public boolean isEmpty(int stackNum) {
-        return arr.size() == 0;
+        return pos[stackNum] == 0;
     }
 }
