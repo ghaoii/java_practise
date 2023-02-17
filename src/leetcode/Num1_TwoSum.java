937ba9c4c7bb4e426e8c82bfcd4f901fd84234e0
ghaoii <641517196@qq.com>
2023-02-17 23:54:56 +0800

    commit_2_17 复习

--- src/leetcode/Num1_TwoSum.java
+++ src/leetcode/Num1_TwoSum.java
@@ -1,17 +1,40 @@
 package leetcode;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Num1_TwoSum {
     public int[] twoSum(int[] nums, int target) {
         int[] ret = new int[2];
-        for (int i = 0; i < nums.length - 1; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                if(nums[i] + nums[j] == target){
-                    ret[0] = i;
-                    ret[1] = j;
-                    break;
-                }
+        if(nums == null || nums.length < 2) {
+            return ret;
+        }
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int n = target - nums[i];
+            if(map.containsKey(n)) {
+                ret[0] = map.get(n);
+                ret[1] = i;
+                break;
             }
+            map.put(nums[i], i);
         }
         return ret;
     }
+
+    //双循环
+//    public int[] twoSum(int[] nums, int target) {
+//        int[] ret = new int[2];
+//        for (int i = 0; i < nums.length - 1; i++) {
+//            for (int j = i + 1; j < nums.length; j++) {
+//                if(nums[i] + nums[j] == target){
+//                    ret[0] = i;
+//                    ret[1] = j;
+//                    break;
+//                }
+//            }
+//        }
+//        return ret;
+//    }
 }
--- src/test/Test.java
+++ src/test/Test.java
@@ -4,48 +4,41 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Test {
-    private static Scanner scanner = new Scanner(System.in);
-//    private static Random random = new Random();
-
     public static void main(String[] args) {
-        System.out.println("猜数字游戏开始！");
-        Random random = new Random();
-        int key = random.nextInt(100) + 1;
-        while(true) {
-            System.out.print("请猜一个数字：");
-            if(!scanner.hasNextInt()) {
-                System.out.println("输入不合法，请重新输入！");
-                scanner.nextLine();
-                continue;
-            }
-            int num = scanner.nextInt();
-            if(key == num) {
-                System.out.println("恭喜你，猜对了！");
-                System.out.println("是否要重新开始猜数字游戏？");
-                if(!restartGame()) {
-                    break;
-                }
-                key = random.nextInt(100) + 1;
-            }else if(key > num) {
-                System.out.println("猜小了");
-            }else {
-                System.out.println("猜大了");
-            }
-        }
-        System.out.println("游戏结束！");
+        System.out.println(fib(6));
     }
 
-    private static boolean restartGame() {
-        scanner.nextLine();
-        while(true) {
-            String str = scanner.nextLine();
-            if ("yes".equals(str)) {
-                return true;
-            } else if ("no".equals(str)) {
-                return false;
-            }
-            System.out.print("输入错误，请重新输入:");
+    private static long sum(int n) {
+        long sum = 0;
+        int fac = 1;
+        for (int i = 1; i <= n; i++) {
+            fac *= i;
+            sum += fac;
         }
+        return sum;
+    }
+
+    private static int max(int a, int b) {
+        return a >= b ? a : b;
+    }
+
+    private static int max(int a, int b, int c) {
+        return max(max(a, b), c);
+    }
+
+    private static int fib(int n) {
+        if(n <= 0) {
+            return 0;
+        }
+
+        int a = 0;
+        int b = 1;
+        for (int i = 0; i < n - 1; i++) {
+            int temp = a + b;
+            a = b;
+            b = temp;
+        }
+        return b;
     }
 }
 
